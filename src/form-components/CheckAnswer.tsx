9801a8fbb964db722ad4b3dacd634a404d4ede62
Sophia Zhang <ssszhang05@gmail.com>
2025-03-09 16:19:10 -0400

    passed all tests for CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
-    expectedAnswer,
+    expectedAnswer
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [ans, setAns] = useState<string>("");
+
+    function updateAns(event: React.ChangeEvent<HTMLInputElement>): void {
+        setAns(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Check Answer</h3>
+            <div>
+                <h3>Check Answer</h3>
+            </div>
+            <Form.Group controlId="answerForm">
+                <Form.Label>Answer: </Form.Label>
+                <Form.Control
+                    type="text"
+                    value={ans}
+                    onChange={updateAns}
+                ></Form.Control>
+            </Form.Group>
+            <div>{ans === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
