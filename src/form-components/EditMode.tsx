57b9832cd9e43e4e784a936c4040d167c3d9925b
Sophia Zhang <ssszhang05@gmail.com>
2025-03-18 20:33:29 -0400

    passed all tests for EditMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,55 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [inEditMode, setEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setStudent] = useState<boolean>(true);
+
+    function switchEditMode(event: React.ChangeEvent<HTMLInputElement>): void {
+        setEditMode(event.target.checked);
+    }
+
+    function updateName(event: React.ChangeEvent<HTMLInputElement>): void {
+        setUserName(event.target.value);
+    }
+
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>): void {
+        setStudent(event.target.checked);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                checked={inEditMode}
+                onChange={switchEditMode}
+            ></Form.Check>
+            {!inEditMode ? (
+                <div>
+                    {userName} is {isStudent ? "" : "not "} a student.
+                </div>
+            ) : (
+                <div>
+                    <Form.Group controlId="studentNameForm">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={userName}
+                            onChange={updateName}
+                        ></Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId="studentForm">
+                        <Form.Label>are you a student?</Form.Label>
+                        <Form.Check
+                            type="checkbox"
+                            checked={isStudent}
+                            onChange={updateStudent}
+                        ></Form.Check>
+                    </Form.Group>
+                </div>
+            )}
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [requests, setRequests] = useState<number>(0);
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>Attempts remaining: {attempts}</div>
+            <Form.Group>
+                <Form.Label>Requested attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requests}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setRequests(
+                            isNaN(parseInt(event.target.value))
+                                ? 0
+                                : parseInt(event.target.value)
+                        );
+                    }}
+                ></Form.Control>
+            </Form.Group>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts > 0 ? attempts - 1 : 0);
+                }}
+                disabled={!attempts}
+            >
+                Use attempt
+            </Button>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts + requests);
+                }}
+            >
+                Gain requested attempt
+            </Button>
         </div>
     );
 }
