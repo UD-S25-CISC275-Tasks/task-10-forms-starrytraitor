a1922a869783756c523d3d5b5e735d79b1627356
Sophia Zhang <ssszhang05@gmail.com>
2025-03-19 12:58:34 -0400

    passed all tests for MCQ

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
-    expectedAnswer,
+    expectedAnswer
 }: {
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [currentChoice, setChoice] = useState<string>(options[0]);
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Select
+                    value={currentChoice}
+                    onChange={(e) => {
+                        setChoice(e.target.value);
+                    }}
+                >
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{currentChoice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
